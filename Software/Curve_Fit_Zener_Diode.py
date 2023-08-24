fd52ca14111a453e62a9e570ea4e7a58e559bc0a
Aarón Vélez <AI.VelezRamirez@gmail.com>
2023-08-24 08:54:43 -0600

    Script to fit DAC2 vs TEMP_CTRL added

---
+++ Software/Curve_Fit_Zener_Diode.py
@@ -0,0 +1,33 @@
+import pandas as pan
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+df = pan.read_excel("Zenner Diode Calibration_Data Template.xlsx")
+
+DAC2_data = df["DAC2"].values
+Trinket_data = df["TEMP_CTRL_MCU"].values
+
+plt.scatter(DAC2_data, Trinket_data)
+plt.show()
+
+def model(Trinket, a, b, c, d):
+    return (a-(b-c*Trinket)**(1/2))/d
+
+initial = [10000, 140000000, 45000, 2000]
+coefficients, covariance = curve_fit(model, Trinket_data, DAC2_data, initial, bounds=(0,np.inf))
+print(coefficients)
+
+Trinket_model = np.linspace(0, 3.3, 100)
+DAC2_model = model(Trinket_model, coefficients[0], coefficients[1], coefficients[2], coefficients[3])
+
+
+plt.scatter(DAC2_data, Trinket_data)
+plt.plot(DAC2_model, Trinket_model, color = 'r')
+plt.show()
+
+
+plt.imshow(np.log(np.abs(covariance)))
+plt.colorbar()
+plt.show()
+
